1532a38778d75ebc11a9424a5295c53669ac3be9
manuel_859 <manuel_859@outlook.es>
2023-08-30 15:53:54 -0600

    validacion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import YouTube from 'react-youtube';
 import './App.css';
 
-
 function App() {
+  const API_URL = 'https://api.themoviedb.org/3'
+  const API_KEY = '352790dd8e3612609ee6e1921115945d'
+  const URL_IMAGE = 'https://image.tmdb.org/t/p/original'
+
+  const [movies, setMovies] = useState([])
+  const [searchKey, setSearchKey] = useState("")
+  const [movie, setMovie] = useState({ title: "Cargando película" });
+
+  const fetchMovies = async (searchKey) => {
+    try {
+      const type = searchKey ? "search" : "discover";
+      const { data: { results } } = await axios.get(`${API_URL}/${type}/movie`, {
+        params: {
+          api_key: API_KEY,
+          query: searchKey,
+        },
+      });
+      setMovies(results);
+      setMovie(results[0]);
+    } catch (error) {
+      alert("Error al cargar las películas:", error);
+      //console.error("Error al cargar las películas:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
-    <div className="App">
-      
+    <div>
+      <div className='container mt-3'>
+        <div className='row'>
+          {movies.map((movie) => (
+            <div key={movie.id} className="col-md-4 mb-3">
+              <img src={`${URL_IMAGE}${movie.poster_path}`} alt="" height={600} width='100%' />
+              <h4 className='text-center'>{movie.title}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
