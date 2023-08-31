3f34fa629a22a3761ab00a10ec580386363a7356
manuel_859 <manuel_859@outlook.es>
2023-08-30 19:08:59 -0600

    funcion para ver informacion de la pelicula

--- src/App.js
+++ src/App.js
@@ -29,15 +29,15 @@ function App() {
     }
   }
 
-//filtro de las peliculas
-const searchMovies = (e)=>{
-  e.preventDefault();
-  fetchMovies(searchKey)
-}
-  
-useEffect(()=>{ 
-  fetchMovies();
-},[]);
+  //filtro de las peliculas
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey)
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   useEffect(() => {
     fetchMovies();
@@ -45,12 +45,37 @@ useEffect(()=>{
 
   return (
     <div>
+      <header style={{ background: '#1C1C1C', color: '#fff', padding: '20px 0', textAlign: 'center' }}>
+        <h1 style={{ fontSize: '3rem', fontWeight: 'bold', marginBottom: '10px' }}>PELIS TRAILERS</h1>
+        <p style={{ fontSize: '1.2rem' }}>La mejor selección de trailers de películas para los amantes del cine.</p>
+      </header>
 
-      <h2 className='text-center mt-5 mb-5'>pelis Trailers </h2>
-      <form className='container mb-4' onSubmit={searchMovies}> 
-        <input type="text" placeholder='Buscar' onChange={(e)=> setSearchKey(e.target.value)} />
-        <button>Buscar</button>
-      </form>
+
+      <h2 className='text-center mt-5 mb-3' style={{ fontSize: '2.8rem', fontWeight: 'bold', color: '#E50914', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)' }}>
+        PELIS TRAILERS
+      </h2>
+
+      <div className='d-flex justify-content-center'>
+        <form className='mt-4' onSubmit={searchMovies}>
+          <div className='input-group'>
+            <input
+              type='text'
+              className='form-control'
+              style={{ maxWidth: '290px' }}
+              placeholder='Buscar'
+              onChange={(e) => setSearchKey(e.target.value)}
+            />
+            <button className='btn btn-primary' type='submit'>
+              Buscar
+            </button>
+          </div>
+        </form>
+      </div>
+
+
+
+
+
       <div className='container mt-3'>
         <div className='row'>
           {movies.map((movie) => (
@@ -61,6 +86,28 @@ useEffect(()=>{
           ))}
         </div>
       </div>
+      {/* Footer */}
+      {/* Footer */}
+      <footer className='text-center mt-5 py-4' style={{ background: '#1C1C1C', color: '#fff' }}>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-4'>
+              <h3>Sobre PELIS TRAILERS</h3>
+              <p>La mejor selección de trailers de películas para los amantes del cine.</p>
+            </div>
+            <div className='col-md-4'>
+              <h3>Contacto</h3>
+              <p>Email: info@pelistrailers.com</p>
+              <p>Teléfono: +1234567890</p>
+            </div>
+            <div className='col-md-4'>
+              <h3>Síguenos en redes</h3>
+              <p>Facebook | Twitter | Instagram</p>
+            </div>
+          </div>
+        </div>
+        <p className='mt-3'>&copy; {new Date().getFullYear()} PELIS TRAILERS. Todos los derechos reservados.</p>
+      </footer>
     </div>
   );
 }
